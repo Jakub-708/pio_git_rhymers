cd7b668308b5e1d0912ba98826767cd113e654b0
Jakub-708 <240708@edu.p.lodz.pl>
2024-05-29 13:01:53 +0200

    17. Generowanie dokumentacji do klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,25 @@
 package edu.kis.vh.nursery;
 
+/**
+ * The FIFORhymer class extends DefaultCountingOutRhymer to implement
+ * a First-In-First-Out (FIFO) counting mechanism.
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * Temporary rhymer used to implement FIFO logic.
+     */
     public final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Retrieves and removes the first element in FIFO order.
+     *
+     * This method overrides the countOut method in DefaultCountingOutRhymer.
+     * It moves all elements to a temporary rhymer and then retrieves the first element.
+     * After that, it restores the remaining elements back to the original rhymer.
+     *
+     * @return the first element in FIFO order
+     */
     @Override
     public int countOut() {
         while (!callCheck())
